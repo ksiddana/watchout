f13ea3fd601828d72ef2a4703282163a729e52a9
ksiddana <ksiddana>
2015-12-12 17:06:34 -0800

    Increase difficulty as score increases.

--- client/watchout.js
+++ client/watchout.js
@@ -1,21 +1,26 @@
 var width = 500,
     height = 500;
+var score = 0;
+var duration = 500;
 
-var duration = 750;
-
-var numberOfEnemies = 4;
+var numberOfEnemies = 10;
 
 //var viewBox = "0 0 500 500"
 
 var color = "green";
 var collisions = 0;
 
+// Selected the body, append an SVG component to the body. Add the Attributes of width
+// Height to the SVG element.
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height)
+    .attr("fill", color);
     //.attr("viewBox", viewBox);
 
+// Select the SVG Element Tag on the HTML Page, and Append a Rectangle to the 
+// Add the Attributes of the Rectange to the Rectangle.
 var rectangle = svg.append("rect")
     .attr("x", 10)
     .attr("y", 10)
@@ -23,27 +28,34 @@ var rectangle = svg.append("rect")
     .attr("height", 500)
     .attr("fill", color);
 
-// Define drag beavior
-function dragmove(d) {
+// Function for 
+// Define drag beavior for the Mouse and the Player. Dragmove returns the X and Y
+// Coordinaes of the Mouse and sets those to the player new cX and cY values by selecting the .player class.
+function dragmove() {
   var x = d3.event.x;
   var y = d3.event.y;
-  d3.event.sourceEvent.preventDefault();
-  //d3.select(".player").attr("transform", "translate(" + (x-250) + "," + (y-250) + ")");
   d3.select(".player").attr("cx", x );
   d3.select(".player").attr("cy", y );
-
-
-  //console.log("x", x,"y", y);
 }
+
+// Define the drag function for the Mouse Behavior which is native to the d3 library.
 var drag = d3.behavior.drag().on("drag", dragmove);
-var point = d3.select(".player")
-var p = { x: point[0], y: point[1] };
 
+// Add a Player Variable and Append it to the SVG Element and give it
+// Attributes of a circle element
+// Style the Circle so that it looks like pointer, when the mouse is over it.
+// We identified the player circle element as a "Class" "Player"
+// Then we also gave it a function called .drag on the player which has to be defined before the player is created and is invoked by moving the player around.
 
+var player = svg.append("circle")
+    .attr("cx", 250)
+    .attr("cy", 250)
+    .attr("r", 15)
+    .attr("fill", "white")
+    .attr("class", "player")
+    .style("cursor", "pointer")
+    .call(drag);
 
-// var detectPosition = function(){
-//   setInterval(function(){console.log(d3.selectAll(".enemy0").attr("cx"));
-//   }, 10); }
 
 
 var detectPlayer = function(){
@@ -59,6 +71,7 @@ var detectPlayer = function(){
 }
 
 
+
 var detectEnemies = function() {
 
    var enemyPosition = [];
@@ -69,70 +82,53 @@ var detectEnemies = function() {
     var eY = d3.select(".enemy" + i).attr("cy");
   
     enemyPosition.push([eX, eY]); 
+    score+= .5;
+    if (duration > 200){
+        duration -= 1;
+    }
+     d3.select(".current").select("span").text(score);
 
    }
 
-   
-
    return enemyPosition;
 }
+
+
 
 var detectCollision = function() {
   
   var playerPosition = detectPlayer();
 
   var enemyPosition = detectEnemies();
-  // console.log(enemyPosition[1]);
 
   for (var i = 0; i < enemyPosition.length; i++){
     var enemyPositionX = enemyPosition[i][0];
     var enemyPositionY = enemyPosition[i][1];
 
-
-
-
     if ((Math.abs(playerPosition[0] - enemyPositionX) < 35 ) && (Math.abs(playerPosition[1] - enemyPositionY) < 35 )) {
 
+      
      collisions++
      console.log("Collision Detected: ", collisions);
+     d3.select(".collisions").select("span").text(collisions);
+     score = 0;
+     duration = 500;
+     d3.select("rect").attr("fill", "aqua");
+     d3.select("rect").transition().duration(500).attr("fill", "green");
+     console.log("Duration: ", duration);
 
+     
     }  
   }
-
-  
-
-
-
-/*  if (Math.abs(playerPosition[0] - enemyPosition[0]) < 15  && Math.abs(playerPosition[1] - enemyPosition[1]) < 15)
-  {
-    console.log("Collision Detected");
-  }*/
-
-  // console.log(playerPosition);
-
-
-
 }
 
-
-var player = svg.append("circle")
-    .attr("cx", 250)
-    .attr("cy", 250)
-    .attr("r", 15)
-    .attr("fill", "white")
-    .attr("class", "player")
-    .style("cursor", "pointer")
-    .call(drag);
-    
-
-    //.call(collide);
-
-
-
-
-    // console.log(player[0])
-
 //ENTER
+// We made a function to place the enemies on the board, using the settings or the pass
+// parameter of the numberOfenemies from the global settings.
+// It creates n number enemies and appends them to the SVG Component using circle elements
+// We also added the attributes of Position of cX adn cY using the Math.random and multiplied it by the board size so that the circles stay within the board.
+// We gave all the enemies unique classes "enemies" + i
+
 function placeEnemies(n) {
   for (var i = 0; i < n; i++) {
     var enemy = svg.append("circle")
@@ -145,28 +141,14 @@ function placeEnemies(n) {
   }
 }
 
-
-
-
-  //check your own position
-  //if your position == (or is close to) player's position
-  //console.log Collision! 
-
-
-
-//POSITION
-
-//select all enemies with a for loop
-  //for each enemy, store position
-  //display position
-
-
-
-
-
+placeEnemies(numberOfEnemies);
 //UPDATE
+
+// Update runs every 1000ms or every second and is invoked by the setInterval function defined below.
 function update() {
 
+  // Iterate through the number of enemies and because we assigned enemies different classes
+  // They had different positions based on Math.random(), since the time is determined by the computer clock. 
   for (var i = 0; i <= numberOfEnemies; i++){
 
     d3.select(".enemy" + i)
@@ -175,36 +157,11 @@ function update() {
         .duration(duration)
         .attr("cx", Math.floor(Math.random() * 500))
         .attr("cy", Math.floor(Math.random() * 500));    
-        
-        // First grab all the enemies cx, and cy positions
-        // Put them in an array
-        // While the values are being overwritten
-          // We want to check if it colides the current position of the Player
-          //
-        //console.log(d3.select('.enemy' + i)[0][0].attributes[0]  
-        //******console.log(d3.select('.enemy' + i).attr("cx"));  
-        // var eX = d3.select('.enemy' + i)[0][0].attributes;
-        // var eX = d3.select('.enemy' + i)[0][0];
-        // var eY = d3.select('.enemy' + i)[0][0];
-        
-
-
-
-
-
-        
-        // console.log("Enemy" + i + " " + eX);
   }
 }
-       
 
-//detectPosition();
-
+// Detect collisions functions is invoked every 250 ms, so that we can capture a collision, while the enemy is moving within 750 ms.       
 setInterval(detectCollision, 250);
 
-placeEnemies(numberOfEnemies);
-
-setInterval(function(){
-  update()
-}, 1000);
+setInterval(update, 1000);
 
